707e66a261f6a514f6710f4333c0a9110b447cf1
MaggiePeterson <margaret.peterson@gmail.com>
2019-04-15 22:43:28 -0700

    fixed hashthresh

--- OpenFilter.cpp
+++ OpenFilter.cpp
@@ -10,96 +10,95 @@
 
 // constructor
 Filter::Filter(void){
-    this->h = new Mat(); // h is hue
-    this->s = new Mat(); // s is saturation
-    this->v = new Mat(); // v is value/brightness
-    h_min = 0;
-    h_max = 0;
-    s_min = 0;
-    s_max = 0;
-    v_min = 0;
-    v_max = 0;
-    
+   this->h = new Mat(); // h is hue
+   this->s = new Mat(); // s is saturation
+   this->v = new Mat(); // v is value/brightness
+   h_min = 0;
+   h_max = 0;
+   s_min = 0;
+   s_max = 0;
+   v_min = 0;
+   v_max = 0;
+
 }
 
 // returns h pointer
 Mat* Filter::getH()
 {
-    return this->h;
+   return this->h;
 }
 
 // returns s pointer
 Mat* Filter::getS()
 {
-    return this->s;
+   return this->s;
 }
 
 // returns v pointer
 Mat* Filter::getV()
 {
-    return this->v;
+   return this->v;
 }
 
 void Filter::writeHSV(string the_file)
 {
-    ofstream myfile;
-    myfile.open (the_file);
-    myfile << h_min << endl << h_max << endl << s_min << endl << s_max
-    << endl << v_min << endl << v_max;
-    myfile.close();
+   ofstream myfile;
+   myfile.open (the_file);
+   myfile << h_min << endl << h_max << endl << s_min << endl << s_max
+   << endl << v_min << endl << v_max;
+   myfile.close();
 }
 
 bool Filter::readHSV(const string &the_file)
 {
-    ifstream inFile(the_file);
-    if (inFile >> h_min >> h_max >> s_min >> s_max >> v_min >> v_max)
-    {
-        createHash(); //set hash values for HSV
-        return true;
-    }
-    return false;
+   ifstream inFile(the_file);
+   if (inFile >> h_min >> h_max >> s_min >> s_max >> v_min >> v_max)
+   {
+      createHash(); //set hash values for HSV
+      return true;
+   }
+   return false;
 }
 
 // converts 1 HSV image to 3 images (one for each H, S, and V values)
 void Filter::createHSV(Mat *img)
 {
-    // variables
-    Mat img_hsv;
+   // variables
+   Mat img_hsv;
 
-    cvtColor(*img, img_hsv, COLOR_BGR2HSV);
-    // save H, S, and V values into separate images
-    std::vector<Mat> channels;
-    split(img_hsv, channels);
-    *this->h = channels[0];
-    *this->s = channels[1];
-    *this->v = channels[2];
+   cvtColor(*img, img_hsv, COLOR_BGR2HSV);
+   // save H, S, and V values into separate images
+   std::vector<Mat> channels;
+   split(img_hsv, channels);
+   *this->h = channels[0];
+   *this->s = channels[1];
+   *this->v = channels[2];
 }
 
 // allows the user to continuously edit the h, s, v min & max threhold values
 void Filter::config(Mat img)
 {
-    createHSV(&img);
+   createHSV(&img);
 
-    resize(img, img, Size(img.cols/3, img.rows/3));
-    const char *windowLocal = "Test";
+   //resize(img, img, Size(img.cols/3, img.rows/3));
+   const char *windowLocal = "Test";
 
-    waitKey(100);
+   waitKey(100);
 
-    waitKey(100);
-    imshow(windowLocal, img);
+   waitKey(100);
+   imshow(windowLocal, img);
 
-    createTrackbar("H min value", windowLocal, &h_min, Filter::MAX_BINARY_VALUE);
-    createTrackbar("H max value", windowLocal, &h_max, Filter::MAX_BINARY_VALUE);
+   createTrackbar("H min value", windowLocal, &h_min, Filter::MAX_BINARY_VALUE);
+   createTrackbar("H max value", windowLocal, &h_max, Filter::MAX_BINARY_VALUE);
 
-    createTrackbar("S min value", windowLocal, &s_min, Filter::MAX_BINARY_VALUE);
-    createTrackbar("S max value", windowLocal, &s_max, Filter::MAX_BINARY_VALUE);
+   createTrackbar("S min value", windowLocal, &s_min, Filter::MAX_BINARY_VALUE);
+   createTrackbar("S max value", windowLocal, &s_max, Filter::MAX_BINARY_VALUE);
 
-    createTrackbar("V min value", windowLocal, &v_min, Filter::MAX_BINARY_VALUE);
-    createTrackbar("V max value", windowLocal, &v_max, Filter::MAX_BINARY_VALUE);
+   createTrackbar("V min value", windowLocal, &v_min, Filter::MAX_BINARY_VALUE);
+   createTrackbar("V max value", windowLocal, &v_max, Filter::MAX_BINARY_VALUE);
 
-    waitKey(100);
+   waitKey(100);
 
-    createHash();
 
 }
 
@@ -109,14 +108,30 @@ void Filter::createHash()
    for(int i =0; i< 255; i++){
 
       if(i > h_min && i < h_max){
-         h_hash[i] =1;
+         h_hash[i] =255;
       }
       if(i > s_min && i < s_max){
-         s_hash[i] =1;
+         s_hash[i] =255;
       }
       if(i > v_min && i < v_max){
-         v_hash[i] =1;
+         v_hash[i] =255;
       }
+   }
+
+   cout<<"H "<<endl;
+   for(int i =0; i< 255; i++){
+      cout<<h_hash[i]<<", ";
+   }
+
+   cout<<"S "<<endl;
+   for(int i =0; i< 255; i++){
+
+      cout<<s_hash[i]<<", ";
+   }
+   cout<<"V "<<endl;
+   for(int i =0; i< 255; i++){
+
+      cout<<v_hash[i]<<", ";
    }
 }
 
@@ -132,92 +147,16 @@ void Filter::thresh(Mat *img, int min_value, int max_value)
 }
 
 void Filter::hashThresh(Mat *img){
+   uint8_t* mover = img->data;
+   uint8_t* end = mover + img->size().width * img->size().height * img->channels();
 
-   int h_range = h_max - h_min;
-   int s_range = s_max - s_min;
-   int v_range = v_max - v_min;
-   int hsv_range[3] = {h_range, s_range, v_range};
-
-   sort(hsv_range, hsv_range + 3);
-   uint8_t* holder = img->data; //do i split into color channels?
-
-   if(hsv_range[0] == h_range){
-
-      for(int i=0; i< img->size().width * img->size().height; i+=3){
-
-         if((holder[i] = h_hash[holder[i]]) < 1){  //if h pixel is not in threshhold
-            holder[i+1] = 0;  //set s to zero
-            holder[i+2] = 0;  //set v to zero
-         }
-         else if(hsv_range[1] == s_range){   //if h is threshhold and s is second restrictive
-            if((holder[i+1] =s_hash[holder[i]]) < 1){  //if s does not meet thresh
-               holder[i+2] = 0;  //set v to zero
-            }
-            else {
-               holder[i+2] = v_hash[holder[i+2]];   //if meets h and s thresh, check v thresh
-            }
-         }
-         else {
-            if((holder[i+2] = v_hash[holder[i+2]])<1){
-               holder[i+1] =0;
-            }
-            else{
-               holder[i+1] =s_hash[holder[i+1]];
-            }
-         }
-      }
-   }
-   else if (hsv_range[0] == s_range){  //if s is most restrictive
-
-      for(int i =1; i< img->size().width * img->size().height; i+=3){
-
-         if((holder[i] = s_hash[holder[i]])<1){  //if s fails
-            holder[i-1] =0;   //set H and v to zero
-            holder[i+1] =0;
-         }
-         else if (hsv_range[1] == h_range){ //if H meets thresh check if H is second most restrictive
-            if((holder[i-1] = h_hash[holder[i-1]]) < 1){ //if H fails, V fails
-               holder[i+1] = 0;
-            }
-            else{
-               holder[i+1] = v_hash[holder[i+1]];
-            }
-         }
-         else{    //if v  is second most restrictive
-            if((holder[i+1] = v_hash[holder[i+1]])<1){
-               holder[i-1] =0;
-            }
-            else{
-               holder[i-1] =h_hash[holder[i-1]];
-            }
-         }
-      }
-   }
-   else{ //v is most restrictive
-
-      for(int i=2; i < img->size().width * img->size().height; i+=3){
-
-         if((holder[i] = v_hash[holder[i]])<1){  //if v fails
-            holder[i-2] =0;   //set H and S to zero
-            holder[i-1] =0;
-         }
-         else if (hsv_range[1] == h_range){ //if H meets thresh check if H is second most restrictive
-            if((holder[i-2] = h_hash[holder[i-2]]) < 1){ //if H fails, s fails
-               holder[i-1] = 0;
-            }
-            else{
-               holder[i-1] = s_hash[holder[i-1]]; //else check s
-            }
-         }
-         else{    //if s  is second most restrictive
-            if((holder[i-1] = s_hash[holder[i-1]])<1){
-               holder[i-2] =0;
-            }
-            else{
-               holder[i-2] =h_hash[holder[i-2]];
-            }
-         }
-      }
+   while(mover< end)
+   {
+      int res = 0;
+      if(h_hash[mover[0]] && s_hash[mover[1]] && v_hash[mover[2]])
+         res = 255;
+      memset(mover, res, 3);
+      mover += 3;
    }
 }
 
@@ -225,10 +164,11 @@ void Filter::hashThresh(Mat *img){
 // then uses Sobel for edge detection
 Mat Filter::edgeDetect(Mat *img)
 {
-    Mat img_hsv;
-    cvtColor(*img, img_hsv, COLOR_BGR2HSV);
-    hashThresh(&img_hsv);
-    return img_hsv;
+   Mat img_hsv, stacked_img;
+   cvtColor(*img, img_hsv, COLOR_BGR2HSV);
+   hashThresh(&img_hsv);
+
+   return img_hsv;
 }
 
 Mat Filter::edgeDetectConfig(Mat *img){
